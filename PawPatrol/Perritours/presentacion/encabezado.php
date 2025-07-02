16e84536f5d3d20d49a535f0f5a4aca7841646c9
CristianBFranco <ccbarreraf@udistrital.edu.co>
2025-07-01 19:36:47 -0500

    logo

--- PawPatrol/Perritours/presentacion/encabezado.php
+++ PawPatrol/Perritours/presentacion/encabezado.php
@@ -1,18 +1,22 @@
 <?php
-  include("presentacion/fondo.php");
+include("presentacion/fondo.php");
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container">
 
-        <a class="navbar-brand fs-4" href="?pid=<?php echo base64_encode("presentacion/inicio.php") ?>">🐾 Perritours</a>
+        <a class="navbar-brand fs-4" href="?pid=<?php echo base64_encode("presentacion/inicio.php") ?>">
+            <img src="imagenes/logo.png" alt="Logo" style="height: 50px; width: auto; margin-right: 10px;">
+            Scooby-Paseo
+        </a>
+
 
         <button class="navbar-toggler" type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarPerritours"
-                aria-controls="navbarPerritours"
-                aria-expanded="false"
-                aria-label="Toggle navigation">
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarPerritours"
+            aria-controls="navbarPerritours"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -33,13 +37,13 @@
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="btn btn-light text-success fw-bold"
-                       href="?pid=<?php echo base64_encode("presentacion/autenticar.php") ?>">
+                        href="?pid=<?php echo base64_encode("presentacion/autenticar.php") ?>">
                         Ingresar
                     </a>
                 </li>
                 <li class="nav-item ms-2">
                     <a class="btn btn-light text-success fw-bold"
-                       href="?pid=<?php echo base64_encode("presentacion/RegistrarUsuario.php"); ?>">
+                        href="?pid=<?php echo base64_encode("presentacion/RegistrarUsuario.php"); ?>">
                         Registrar Usuario
                     </a>
                 </li>
